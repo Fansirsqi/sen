6aa3496eca0c3972d8b9fcb2d98d48bf645b8369
fansir <fansirliu@foxmail.com>
2025-09-05 18:49:22 +0800

    feat(core): 添加PackageHelper类用于支付宝包名和类名管理

---
+++ app/src/main/java/com/fansirsqi/sen/util/PackageHelper.kt
@@ -0,0 +1,10 @@
+package com.fansirsqi.sen.util
+
+object PackageHelper {
+    /**支付宝客户端的包名。*/
+    const val TARGET_PACKAGE: String = "com.eg.android.AlipayGphone"
+
+
+    /**当前使用的服务类名。*/
+    const val CURRENT_USING_SERVICE: String = "com.alipay.dexaop.power.RuntimePowerService"
+}
